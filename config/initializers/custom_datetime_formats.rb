50b24768596d4d1d552f8ec6a5baaf434ec5a004
Scott Wolff <scott.wolff@tuscan.ca>
2009-11-19 16:01:49 -0700

    adds custom date formats

---
+++ config/initializers/custom_datetime_formats.rb
@@ -0,0 +1,11 @@
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
+  :short => "%Y/%m/%d",
+  :med => "%d-%b-%Y",
+  :long => "%A %B %d, %Y",
+  :military => "%H%M"  
+)
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+  :military => "%H%M",
+  :short => "%I:%M %p"  
+)
