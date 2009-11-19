cfb831b73e9a7921b27285fa45a2a18ab63de855
Scott Wolff <scott.wolff@tuscan.ca>
2009-11-19 16:00:59 -0700

    sort task table

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   attr_accessor :end_session
   
-  named_scope :today, lambda { {:conditions => ["DATE(start) >= #{Date.today}"]} }
+  named_scope :today, lambda { {:conditions => ["DATE(start) >= #{Date.today}"], :order => "start DESC"} }
 end
