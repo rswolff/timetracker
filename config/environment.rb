1c47ce8b2b5c644008c6e593539d68a2849a6183
Scott Wolff <scott.wolff@tuscan.ca>
2009-11-21 16:39:08 -0700

    added restfull authentication

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "acts-as-taggable-on", :source => "http://gemcutter.org"
+  config.gem "acts-as-taggable-on", :source => "http://gemcutter.org" 
+  config.gem "aasm", :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -31,6 +32,8 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  config.active_record.observers = :user_observer
+  
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
