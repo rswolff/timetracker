02d10222ae2753c575bbe94c67020e4560b044ae
Scott Wolff <scott.wolff@tuscan.ca>
2009-11-24 01:21:10 -0700

    some task cleanup

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ActiveRecord::Base
   
   belongs_to :user
   
+  validates_presence_of :start, :stop, :notes
+  
   attr_accessor :end_session, :tags
   named_scope :today, lambda {{:conditions => ["DATE(start) = '#{Time.zone.today}'"], :order => "start DESC"}}
 end
